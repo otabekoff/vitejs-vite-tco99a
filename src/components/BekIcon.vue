3699162696cced117db4a5ca5c6ec15566fd61c5
otabekoff <60849894+otabekoff@users.noreply.github.com>
2024-08-17 09:22:55 +0500

    Initial commit

---
+++ src/components/BekIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg class="icon" :class="className" :width="size" :height="size">
+    <use :xlink:href="`${props.icon}`"></use>
+  </svg>
+</template>
+
+<script setup>
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: [String, Number],
+    default: '24'
+  },
+  className: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<style scoped>
+.icon {
+  display: inline-block;
+  vertical-align: middle;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <main>
+    Hey
+    <bek-icon icon="/ui-icons.svg#hamburger" />
+    <bek-icon icon="/ui-icons.svg#search" />
+    <bek-icon icon="/ui-icons.svg#play" />
+    <bek-icon icon="/ui-icons.svg#dots-vert" />
+
+    <!-- Uncomment below line -->
+    <!-- <bek-icon icon="/ui-icons.svg#bookmark" /> -->
+
+    <bek-icon icon="/ui-icons.svg#notification" />
+  </main>
+</template>
